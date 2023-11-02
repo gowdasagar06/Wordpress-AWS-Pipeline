cd2ad1797538864434acf401f0a502e801b77731
Sagar <sagargowda6666@gmail.com>
2023-11-02 09:24:18 +0000

    Edited install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 # sudo apt install apache2 -y
 # sudo apt install php libapache2-mod-php php-mysql -y
 # sudo apt update -y
@@ -7,5 +7,5 @@
 # sudo a2enmod rewrite
 # sudo apache2ctl configtest
 # sudo systemctl restart apache2
-# sudo unzip /home/ubuntu/wordpress.zip 2>/dev/null
-sudo cp -R /opt/codedeploy-agent/deployment-root/c8cd40ab-e5d5-43f4-b338-b4e787fe346f/d-FQXZOB1F1/deployment-archive/wordpress /var/www/
+sudo unzip /home/ubuntu/wordpress.zip 2>/dev/null
+sudo cp -R /home/ubuntu/wordpress /wordpress /var/www/ 2>/dev/null
