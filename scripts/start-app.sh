954e84f0cd86bf58c87cb2aad779a4fd42d0113a
sagar <sagargowda6666@gmail.com>
2023-11-02 11:17:14 +0000

    Edited start-app.sh

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo chown -R www-data:www-data /var/www/wordpress
-sudo find /var/www/wordpress/ -type d -exec chmod 750 {} \;
-sudo find /var/www/wordpress/ -type f -exec chmod 640 {} \;
-sudo systemctl restart apache2
+chown -R www-data:www-data /var/www/wordpress
+find /var/www/wordpress/ -type d -exec chmod 750 {} \;
+find /var/www/wordpress/ -type f -exec chmod 640 {} \;
+systemctl restart apache2
