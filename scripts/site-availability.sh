345b121dfd009fe6aa37cf4f6a747c618c621530
application server-sg <sagargowda6666@gmail.com>
2023-11-02 10:31:12 +0000

    Edited site-availability.sh

--- scripts/site-availability.sh
+++ scripts/site-availability.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo unzip /home/ubuntu/wordpress.zip 2>/dev/null
 sudo cp -R /home/ubuntu/wordpress /var/www/ 2>/dev/null
 sudo rm -rf /etc/apache2/sites-available/000-default.conf
 sudo cp /var/www/wordpress/000-default.conf /etc/apache2/sites-available/
