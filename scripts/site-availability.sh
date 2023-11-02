be649d366de75000903efbb4a3566e23823af2a1
tt <sagargowda6666@gmail.com>
2023-11-02 11:16:44 +0000

    Edited site-availability.sh

--- scripts/site-availability.sh
+++ scripts/site-availability.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo cp -R /home/ubuntu/wordpress /var/www/ 2>/dev/null
-sudo rm -rf /etc/apache2/sites-available/000-default.conf
-sudo cp /var/www/wordpress/000-default.conf /etc/apache2/sites-available/
+cp -R /home/ubuntu/wordpress /var/www/ 2>/dev/null
+rm -rf /etc/apache2/sites-available/000-default.conf
+cp /var/www/wordpress/000-default.conf /etc/apache2/sites-available/
