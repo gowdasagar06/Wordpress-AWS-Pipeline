96a57d9fd34d3b0a65a811b25b38db568cb12c86
Sagar <sagargowda6666@gmail.com>
2023-11-02 06:37:39 +0000

    Edited install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php php-mysql -y
-sudo apt update -y
-sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
-sudo systemctl start apache2
-sudo a2enmod rewrite
-sudo apache2ctl configtest
-sudo systemctl restart apache2
+# #!/bin/bash
+# sudo apt install apache2 -y
+# sudo apt install php libapache2-mod-php php-mysql -y
+# sudo apt update -y
+# sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+# sudo systemctl start apache2
+# sudo a2enmod rewrite
+# sudo apache2ctl configtest
+# sudo systemctl restart apache2
 # sudo unzip /home/ubuntu/wordpress.zip 2>/dev/null
 sudo cp -R /home/ubuntu/ /var/www/
